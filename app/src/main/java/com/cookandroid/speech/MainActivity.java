345348c67f75891a16d4f735cfb39fa38869625e
hijerry17 <hijerry17@naver.com>
2020-04-21 06:29:55 +0900

    recognition

---
+++ app/src/main/java/com/cookandroid/speech/MainActivity.java
@@ -0,0 +1,171 @@
+package com.cookandroid.speech;
+
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    Intent i;
+    SpeechRecognizer mRecognizer;
+    Button btn;
+    TextView tv1, tv2, speech, tv_result;
+    int PERMISSION;
+    String[] result = new String[5];
+    String[] example = new String[5];
+    int count;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        tv1 = findViewById(R.id.tv1);
+        tv2 = findViewById(R.id.tv2);
+        tv_result = findViewById(R.id.tv_result);
+        speech = findViewById(R.id.speech);
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            // 퍼미션 체크
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET,
+                    Manifest.permission.RECORD_AUDIO}, PERMISSION);
+        }
+
+            i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            i.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
+            i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR");
+
+            createData();
+            count = -1;
+
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    count++;
+                    tv1.setText(example[count]);
+                    mRecognizer = SpeechRecognizer.createSpeechRecognizer(getApplicationContext());
+                    mRecognizer.setRecognitionListener(listener);
+                    mRecognizer.startListening(i);
+                }
+            });
+
+    }
+
+    RecognitionListener listener = new RecognitionListener() {
+
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+            System.out.println("onReadyForSpeech.........................");
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+            speech.setTextColor(Color.parseColor("#FFA7D5FF"));
+            Toast.makeText(getApplicationContext(), "지금 말해주세요.", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onRmsChanged(float rmsdB) {
+            System.out.println("onRmsChanged.........................");
+        }
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {
+            System.out.println("onBufferReceived.........................");
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            System.out.println("onEndOfSpeech.........................");
+            speech.setTextColor(Color.parseColor("#000000"));
+
+        }
+
+        @Override
+        public void onError(int error) {
+            String message;
+
+            switch (error) {
+                case SpeechRecognizer.ERROR_AUDIO:
+                    message = "오디오 에러";
+                    break;
+                case SpeechRecognizer.ERROR_CLIENT:
+                    message = "클라이언트 에러";
+                    break;
+                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                    message = "퍼미션 없음";
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK:
+                    message = "네트워크 에러";
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                    message = "네트웍 타임아웃";
+                    break;
+                case SpeechRecognizer.ERROR_NO_MATCH:
+                    message = "찾을 수 없음";
+                    break;
+                case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                    message = "RECOGNIZER가 바쁨";
+                    break;
+                case SpeechRecognizer.ERROR_SERVER:
+                    message = "서버가 이상함";
+                    break;
+                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                    message = "말하는 시간초과";
+                    break;
+                default:
+                    message = "알 수 없는 오류임";
+                    break;
+            }
+
+            Toast.makeText(getApplicationContext(), "에러가 발생하였습니다. : " + message,Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPartialResults(Bundle partialResults) {
+            System.out.println("onPartialResults.........................");
+        }
+
+        @Override
+        public void onEvent(int eventType, Bundle params) {
+            System.out.println("onEvent.........................");
+        }
+
+        @Override
+        public void onResults(Bundle results) {
+            String key = "";
+            key = SpeechRecognizer.RESULTS_RECOGNITION;
+            ArrayList<String> mResult = results.getStringArrayList(key);
+            String[] rs = new String[mResult.size()];
+            mResult.toArray(rs);
+            tv2.setText(rs[0]);
+            result[count] = rs[0];
+            tv_result.setText(count+":"+result[count]);
+        }
+
+    };
+
+    public void createData() {
+        example[0] = "안녕하세요.";
+        example[1] = "음성 인식 테스트입니다.";
+        example[2] = "생각보다 복잡해지네요.";
+        example[3] = "이거 안되면 나 치킨 시킬거다.";
+        example[4] = "배고파서 아님 속상해서임.";
+    }
+
+}
+
+
+
